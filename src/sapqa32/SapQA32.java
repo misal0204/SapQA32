085c696572a3a38653bd4888239e945b1050e00f
hajfec12 <hajfec12@pcmisaelsv.division.local>
2016-02-10 10:57:18 -0600

    primera versión de QA32SAP

---
+++ src/sapqa32/SapQA32.java
@@ -0,0 +1,16 @@
+package sapqa32;
+
+/**
+ *
+ * @author Misael Recinos
+ */
+public class SapQA32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
